5e523f868fe0a894caa0b8b0180f89b47ad2e4d5
Muhammadaziz <salohiddinov.mm@gmail.com>
2024-02-28 22:52:06 +0500

    redirects edited

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    workboxOptions: {
-      exclude: [/_redirects/]
-    }
-  },
   publicPath: process.env.NODE_ENV === "production" ? "" : "/",
 });
